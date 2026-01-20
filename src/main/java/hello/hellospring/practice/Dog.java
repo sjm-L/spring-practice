a30ef5e0c558739c1fae7b0b00fe932b243ffaf8
sjm-L <shower1300@gmail.com>
2026-01-20 15:06:37 +0900

    practice

---
+++ src/main/java/hello/hellospring/practice/Dog.java
@@ -0,0 +1,13 @@
+package hello.hellospring.practice;
+
+public class Dog {
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String speak() {
+        return "멍!";
+    }
+}
